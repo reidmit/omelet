930cb4ae45b1da3a91ad8fd5edf2e33e923b505f
reid47 <rbm02012@mymail.pomona.edu>
2016-05-04 05:19:56 -0700

    Fixed some path names

--- run.js
+++ run.js
@@ -3,14 +3,14 @@ var Toast = require('./toast.js');
 var T = new Toast.ToastInstance({
     sourceLanguage: "omelet",
     targetLanguage: "html",
-    outputDirectory: "ignored/docs2/outputs",
+    outputDirectory: "ignored/omelet",
     prettyPrint: true,
     isWeb: false
 })
 
-var ctx = require("./ignored/docs2/templates/config.json");
+var ctx = require("./ignored/omelet/templates/config.json");
 
-T.renderDirectory("ignored/docs2", ctx);
+T.renderDirectory("ignored/omelet", ctx);
 // var output = T.renderFile("ignored/docs2/filters.oml", ctx);
 // console.log("OUTPUT:")
 // console.log(output);
