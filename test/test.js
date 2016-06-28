f1fb32d9d255f3e85e6b4b6ee54bb184f8600be7
reid47 <rbm02012@mymail.pomona.edu>
2016-06-28 18:13:24 -0400

    Rewriting tests, adding coverage with Blanket

--- test/test.js
+++ test/test.js
@@ -1,18 +1,52 @@
 var assert = require('assert')
 
-var indentation = require('../lib/indentation.js')
+var lexer = require('../lib/lexer.js')
 var parser = require('../lib/parser.js')
-var evaluate = require('../lib/evaluate.js')
+var linker = require('../lib/linker.js')
+var compiler = require('../lib/compiler.js')
 
-function omeletToHtml(input, context) {
+function omeletToHtml(input, context, options) {
     context = context || {}
-    var ast = parser.parse(indentation.preprocess(input))
-    var output = evaluate(ast, input, context, {
-        directory: '.',
-        file: '.'
-    })
-    return output
+    options = options || {}
+    var tokens = lexer.lex(input, options),
+        ast = parser.parse(tokens, options),
+        linkedAst = linker.link(ast, options),
+        template = compiler.compile(linkedAst, options)
+    return template(context)
 }
+
+describe('Tags', function() {
+    var context = {},
+        options = {
+            prettyPrint: false
+        },
+        examples
+
+    examples = [
+        {
+            name: 'plain tag',
+            input: '@h1 hello, world!',
+            output: '<h1>hello, world!</h1>'
+        },
+        {
+            name: 'several tags',
+            input: '@h1 hello, world!\n@h2 hi!\n@h3 this is omelet',
+            output: '<h1>hello, world!</h1><h2>hi!</h2><h3>this is omelet</h3>'
+        },
+        {
+            name: 'tags with attributes',
+            input: '@a#anchor.className[href="#" target="_blank"] click me',
+            output: '<a class="className" id="anchor" href="#" target="_blank">click me</a>'
+        },
+    ]
+
+    examples.forEach(function(example) {
+        it('translates ' + example.name, function() {
+            assert.equal(omeletToHtml(example.input), example.output)
+        })
+    })
+
+})
 
 // describe("Omelet tags to HTML tags", function() {
 //     var context = {};
@@ -81,207 +115,3 @@ function omeletToHtml(input, context) {
 //         });
 //     });
 // });
-
-describe('Object and array definitions', function() {
-
-    var ex_simple_num = [
-        {name: 'positive integer',
-            input: '+num: 47',
-            output: ''},
-        {name: 'negative integer',
-            input: '+num: -47',
-            output: ''},
-        {name: 'positive float',
-            input: '+num: 47.1223',
-            output: ''},
-        {name: 'negative float',
-            input: '+num: -47.1223',
-            output: ''}
-    ]
-    ex_simple_num.forEach(function(example) {
-        it('shouldn\'t output anything for number definitions: ' + example.name, function() {
-            assert.equal(omeletToHtml(example.input), example.output)
-        })
-    })
-
-    var ex_simple_str = [
-        {name: 'double-quoted string',
-            input: '+name: "reid mitchell"',
-            output: ''},
-        {name: 'single-quoted string',
-            input: '+name: \'reid mitchell\'',
-            output: ''},
-        {name: 'empty string',
-            input: '+name: \'\'',
-            output: ''}
-    ]
-    ex_simple_str.forEach(function(example) {
-        it('shouldn\'t output anything for string definitions: ' + example.name, function() {
-            assert.equal(omeletToHtml(example.input), example.output)
-        })
-    })
-
-    var ex_simple_bool = [
-        {
-            name: 'boolean true',
-            input: '+isOmeletCool: true',
-            output: ''
-        },
-        {
-            name: 'boolean false',
-            input: '+isOmeletUncool: false',
-            output: ''
-        }
-    ]
-    ex_simple_bool.forEach(function(example) {
-        it('shouldn\'t output anything for boolean definitions: ' + example.name, function() {
-            assert.equal(omeletToHtml(example.input), example.output)
-        })
-    })
-
-    var ex_array = [
-        {
-            name: 'array of numbers',
-            input: '+arr:\n' +
-                   '  - 47\n' +
-                   '  - 100\n' +
-                   '  - 28',
-            output: ''
-        },
-        {
-            name: 'array of strings',
-            input: '+arr:\n' +
-                   '  - \'hello, world\'\n' +
-                   '  - \'this is Omelet!\'\n' +
-                   '  - \'lorem ipsum\'',
-            output: ''
-        },
-        {
-            name: 'array of booleans',
-            input: '+arr:\n' +
-                   '  - true\n' +
-                   '  - true\n' +
-                   '  - false',
-            output: ''
-        },
-        {
-            name: 'array of arrays',
-            input: '+arr:\n' +
-                   '  -\n' +
-                   '    - 1\n' +
-                   '    - 2\n' +
-                   '    - 3\n' +
-                   '  -\n' +
-                   '    - \'a\'\n' +
-                   '    - \'b\'\n' +
-                   '    - \'c\'',
-            output: ''
-        },
-
-        {
-            name: 'mixed array',
-            input: '+arr:\n' +
-                   '  - 47\n' +
-                   '  - \'hello, world\'\n' +
-                   '  - false\n' +
-                   '  -\n' +
-                   '    - 1\n' +
-                   '    - \'hello\'',
-            output: ''
-        }
-    ]
-    ex_array.forEach(function(example) {
-        it('shouldn\'t output anything for array definitions: ' + example.name, function() {
-            assert.equal(omeletToHtml(example.input), example.output)
-        })
-    })
-
-})
-
-describe('Filters', function() {
-    var context = {
-        text: 'Omelet',
-        num: 47,
-        fruits: ['apple', 'banana', 'orange'],
-        person: {
-            name: 'Jane Doe',
-            age: 22
-        },
-        people: [
-            {name: 'Alice', age: 22},
-            {name: 'Bob', age: 47},
-            {name: 'Carl', age: 3},
-            {name: 'Dani', age: 65}
-        ],
-        uh_oh: '<script>console.log(\'bad!\');</script>',
-        extra_spaces: '   hello      ',
-        list_str: 'hey,how,are,you,doing?'
-    }
-
-    var ex_applied = [
-        {
-            name: '{text|uppercase}',
-            input: '{text|uppercase}',
-            output: 'OMELET'
-        },
-        {
-            name: '{text|replace \'e\' \'o\'}',
-            input: '{text|replace \'e\' \'o\'}',
-            output: 'Omolot'
-        },
-        {
-            name: '{text|replace \'e\' \'o\'|uppercase}',
-            input: '{text|replace \'e\' \'o\'|uppercase}',
-            output: 'OMOLOT'
-        },
-        {
-            name: '{text|uppercase|replace \'e\' \'o\'}',
-            input: '{text|uppercase|replace \'e\' \'o\'}',
-            output: 'OMELET'
-        }
-    ]
-    ex_applied.forEach(function(example) {
-        it('should apply to interpolations: ' + example.name, function() {
-            assert.equal(omeletToHtml(example.input, context), example.output)
-        })
-    })
-
-
-    var ex_filters = [
-        {
-            name: 'escape',
-            input: '{uh_oh|escape}',
-            output: '&lt;script&gt;console.log(&amp;apos;bad!&amp;apos;);&lt;/script&gt;'
-        },
-        {
-            name: 'uppercase',
-            input: '{text|uppercase}',
-            output: 'OMELET'
-        },
-        {
-            name: 'uppercase (non-string)',
-            input: '{num|uppercase}',
-            output: '47'
-        },
-        {
-            name: 'lowercase',
-            input: '{text|lowercase}',
-            output: 'omelet'
-        },
-        {
-            name: 'lowercase (non-string)',
-            input: '{num|lowercase}',
-            output: '47'
-        },
-        {
-            name: 'trim',
-            input: '{extra_spaces|trim}',
-            output: 'hello'
-        }
-    ]
-    ex_filters.forEach(function(example) {
-        it('should apply correctly: ' + example.name, function() {
-            assert.equal(omeletToHtml(example.input, context), example.output)
-        })
-    })
-})
