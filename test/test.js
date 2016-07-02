c5052e7e91db39eb215575a73b0fd325e7dcdf5b
reid47 <rbm02012@mymail.pomona.edu>
2016-07-02 16:53:41 -0400

    Fixing filters, adding filtr tests

--- test/test.js
+++ test/test.js
@@ -38,6 +38,11 @@ describe('Tags', function() {
             input: '@a#anchor.className[href="#" target="_blank"] click me',
             output: '<a class="className" id="anchor" href="#" target="_blank">click me</a>'
         },
+        {
+            name: 'multitags',
+            input: '@b|i.classy|u testing',
+            output: '<b><i class="classy"><u>testing</u></i></b>'
+        }
     ]
 
     examples.forEach(function(example) {
@@ -45,73 +50,146 @@ describe('Tags', function() {
             assert.equal(omeletToHtml(example.input), example.output)
         })
     })
-
 })
 
-// describe("Omelet tags to HTML tags", function() {
-//     var context = {};
+describe('Filters', function() {
+    var context = {
+            word: 'omelet',
+            sentence: 'Omelet is cool!',
+            number: 47,
+            listOfNumbers: [4, 8, 15, 16, 23, 42],
+            listOfMixed: [true, 47, 'hello', 23, 42, 0],
+            listShort: ['lol'],
+            listOfObjects: [
+                {
+                    name: 'book',
+                    price: 20
+                },
+                {
+                    name: 'sandwich',
+                    price: 7
+                },
+                {
+                    name: 'soda',
+                    price: 2
+                }
+            ],
+            object: {
+                price: 20,
+                name: 'book'
+            }
+        },
+        options = {
+            prettyPrint: false
+        }
+    var examples = [
+        {
+            name: 'eq',
+            input: '{word | eq "omelet"}, {word | eq 47}, {number | eq 47}, {object | eq object}, {object | eq listOfObjects[0]}, {listOfMixed | eq listShort}, {listOfMixed | eq listOfNumbers}',
+            output: 'true, false, true, true, true, false, false'
+        },
+        {
+            name: 'neq',
+            input: '{word | neq "omelet"}, {word | neq 47}, {number | neq 47}, {object | neq object}, {object | neq listOfObjects[0]}, {listOfMixed | neq listShort}, {listOfMixed | neq listOfNumbers}',
+            output: 'false, true, false, false, false, true, true'
+        },
+    ]
 
-//     var ex_lineTag = [
-//         {name: "h1",
-//             input: "@h1 hello, world!",
-//             output: "<h1>hello, world!</h1>"},
-//         {name: "a",
-//             input: "@a[href=\"#\" target=\"_blank\"] a link",
-//             output: "<a href=\"#\" target=\"_blank\">a link</a>"},
-//         {name: "b|i|u",
-//             input: "@b|i|u some text",
-//             output: "<b><i><u>some text</u></i></b>"},
-//         {name: "b.className|i",
-//             input: "@b.className|i some text",
-//             output: "<b class=\"className\"><i>some text</i></b>"},
-//     ];
+    examples.forEach(function(example) {
+        it('correctly applies filter ' + example.name, function() {
+            assert.equal(omeletToHtml(example.input, context), example.output)
+        })
+    })
+})
 
-//     ex_lineTag.forEach(function(example) {
-//         it('translates line tag: '+example.name, function() {
-//             assert.equal(omeletToHtml(example.input), example.output);
-//         });
-//     });
+describe('Doctypes', function() {
+    var context = {},
+        options = {
+            prettyPrint: false
+        },
+        examples
 
-//     var ex_interpolatedTag = [
-//         {name: "{@b}",
-//             input: "this one has {@b bold text}.",
-//             output: "this one has <b>bold text</b>."},
-//         {name: "@h1 {@b}",
-//             input: "@h1 this title has {@b bold text}.",
-//             output: "<h1>this title has <b>bold text</b>.</h1>"},
-//         {name: "{@b|i}",
-//             input: "this has {@b|i interpolated multitags}!",
-//             output: "this has <b><i>interpolated multitags</i></b>!"}
-//     ];
+    examples = [
+        {
+            name: 'html',
+            input: '@doctype html',
+            output: '<!doctype html>'
+        },
+        {
+            name: 'html5',
+            input: '@doctype html5',
+            output: '<!doctype html>'
+        },
+        {
+            name: '5',
+            input: '@doctype 5',
+            output: '<!doctype html>'
+        },
+        {
+            name: '4.01',
+            input: '@doctype 4.01',
+            output: '<!DOCTYPE HTML PUBLIC \'-//W3C//DTD HTML 4.01 Transitional //EN\' \'http://www.w3.org/TR/html4/loose.dtd\'>'
+        },
+        {
+            name: 'transitional',
+            input: '@doctype 4.01',
+            output: '<!DOCTYPE HTML PUBLIC \'-//W3C//DTD HTML 4.01 Transitional //EN\' \'http://www.w3.org/TR/html4/loose.dtd\'>'
+        },
+        {
+            name: 'frameset',
+            input: '@doctype frameset',
+            output: '<!DOCTYPE HTML PUBLIC \'-//W3C//DTD HTML 4.01 Frameset //EN\' \'http://www.w3.org/TR/html4/frameset.dtd\'>'
+        },
+        {
+            name: 'xhtml_1.0',
+            input: '@doctype xhtml_1.0',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Strict //EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\'>'
+        },
+        {
+            name: 'xhtml_1.0_strict',
+            input: '@doctype xhtml_1.0_strict',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Strict //EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\'>'
+        },
+        {
+            name: 'xhtml_strict',
+            input: '@doctype xhtml_strict',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Strict //EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\'>'
+        },
+        {
+            name: 'xhtml_1.0_transitional',
+            input: '@doctype xhtml_1.0_transitional',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Transitional //EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\'>'
+        },
+        {
+            name: 'xhtml_transitional',
+            input: '@doctype xhtml_transitional',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Transitional //EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\'>'
+        },
+        {
+            name: 'xhtml_1.0_frameset',
+            input: '@doctype xhtml_1.0_frameset',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Frameset //EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\'>'
+        },
+        {
+            name: 'xhtml_frameset',
+            input: '@doctype xhtml_frameset',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.0 Frameset //EN\' \'http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd\'>'
+        },
+        {
+            name: 'xhtml_1.1',
+            input: '@doctype xhtml_1.1',
+            output: '<!DOCTYPE html PUBLIC \'-//W3C//DTD XHTML 1.1//EN\' \'http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\'>'
+        },
+        {
+            name: 'something_else',
+            input: '@doctype something_else',
+            output: '<!doctype something_else>'
+        }
+    ]
 
-//     ex_interpolatedTag.forEach(function(example) {
-//         it('translates interpolated tag: '+example.name, function() {
-//             assert.equal(omeletToHtml(example.input), example.output);
-//         });
-//     });
-// });
-
-//these tests pass, but they aren't parsed as numbers, just as strings
-// describe("Omelet literals to HTML", function() {
-//     var context = {};
-
-//     var ex_numbers_ok = [
-//         {name: "positive integer",
-//             input: "47",
-//             output: "47"},
-//         {name: "negative integer",
-//             input: "-47",
-//             output: "-47"},
-//         {name: "positive decimal",
-//             input: "47.4747",
-//             output: "47.4747"},
-//         {name: "negative decimal",
-//             input: "-47.4747",
-//             output: "-47.4747"}
-//     ];
-//     ex_numbers_ok.forEach(function(example) {
-//         it('translates number: '+example.name, function() {
-//             assert.equal(omeletToHtml(example.input), example.output);
-//         });
-//     });
-// });
+    examples.forEach(function(example) {
+        it('translates ' + example.name, function() {
+            assert.equal(omeletToHtml(example.input), example.output)
+        })
+    })
+})
