426cc79b4f9561984e340173c9a1b48fe39c9ff0
Reid Mitchell <rbm02012@mymail.pomona.edu>
2017-01-21 00:53:31 -0800

    Small tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,6 @@ module.exports = {
             {'skipBlankLines': true }
         ],
         'no-unneeded-ternary': 'error',
-        'no-var': 'error',
         'quotes': [
             'error',
             'single'
